14ce5a3b0163e7854a7dfcfb577aeb64fe9d46da
Thomas Clark <thomas.clark@bigtincan.com>
2023-09-22 22:14:17 +1000

    refactored slide out menu to svelte component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
 import vanillaExtract from "astro-vanilla-extract";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
   scopedStyleStrategy: "class",
-  integrations: [vanillaExtract()],
+  integrations: [vanillaExtract(), svelte()],
 });
